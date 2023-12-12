9c29dfced9d78f2e540914fa2608ffb0f05f48f5
Lady Killerr <dungvuduc0509@gmail.com>
2023-12-12 23:12:59 +0700

    modified: update drag value, fixed the bug conflicting with obstacles

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,7 +9,7 @@ public class Movement : MonoBehaviour
 
     [SerializeField] float moveAmount = 10f;
     [SerializeField] float rotationAmount = 20f;
-    float rotationSides = 1f;
+
 
     Rigidbody rocketRigidbody;
 
@@ -29,10 +29,10 @@ public class Movement : MonoBehaviour
     void ProcessThrust()
     {
         // Thrusting
-        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
             rocketRigidbody.AddRelativeForce(Vector3.up * moveAmount, ForceMode.Force);
-        } 
+        }
     }
 
     void ProcessRotation()
@@ -40,12 +40,19 @@ public class Movement : MonoBehaviour
         // Rotate on the right
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
+            rocketRigidbody.freezeRotation = true; // freeze physics system so we can manually rotate
             rocketRigidbody.AddTorque(Vector3.forward * rotationAmount, ForceMode.Force);
+            rocketRigidbody.freezeRotation = false; // unfreeze physics system so we can let unity take over after we r done manually rotating
+
         }
         // Rotate on the left
         else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            rocketRigidbody.AddTorque(Vector3.forward * -1 * rotationAmount, ForceMode.Force);
+            rocketRigidbody.freezeRotation = true;
+            rocketRigidbody.AddTorque(Vector3.forward * -rotationAmount, ForceMode.Force);
+            rocketRigidbody.freezeRotation = false;
+
         }
+
     }
 }
